5e61dbe56912842f43bf4aa4b20cf2939b00363d
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-06-25 16:43:24 -0300

    Using validated env variables

--- src/lib/authProvider.ts
+++ src/lib/authProvider.ts
@@ -1,11 +1,8 @@
 import { StaticAuthProvider } from "@twurple/auth";
-import { env } from "../env";
-
-const clientId = env.CLIENT_ID;
-const accessToken = env.ACCESS_TOKEN
+import { CLIENT_ID, ACCESS_TOKEN } from "../env";
 
 // As a minor optimization, you may pass the scopes of the token, but be sure they're correct in that case!
-export const authProvider = new StaticAuthProvider(clientId, accessToken, [
+export const authProvider = new StaticAuthProvider(CLIENT_ID, ACCESS_TOKEN, [
   "channel:read:redemptions",
   "channel:manage:redemptions",
 ]);

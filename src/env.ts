53b6048ae206e2064a4f4a61750e95951944092c
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-06-25 17:05:36 -0300

    Reward redemptions listener

--- src/env.ts
+++ src/env.ts
@@ -4,8 +4,10 @@ import { z } from "zod";
 
 config();
 
-export const { CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN } = parseEnv(process.env, {
+export const { CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN, BROADCASTER_ID, REWARD_ID } = parseEnv(process.env, {
   CLIENT_ID: z.string().min(1),
   CLIENT_SECRET: z.string().min(1),
   ACCESS_TOKEN: z.string().min(1),
+  BROADCASTER_ID: z.string().min(1),
+  REWARD_ID: z.string().min(1),
 });

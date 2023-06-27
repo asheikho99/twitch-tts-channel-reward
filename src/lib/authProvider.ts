3d8547ce28be015888bc2a0d3d31ee353e583732
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-06-27 13:21:04 -0300

    Using Sveltekit skeleton template

--- src/lib/authProvider.ts
+++
@@ -1,8 +0,0 @@
-import { StaticAuthProvider } from "@twurple/auth";
-import { CLIENT_ID, ACCESS_TOKEN } from "../env";
-
-// As a minor optimization, you may pass the scopes of the token, but be sure they're correct in that case!
-export const authProvider = new StaticAuthProvider(CLIENT_ID, ACCESS_TOKEN, [
-  "channel:read:redemptions",
-  "channel:manage:redemptions",
-]);

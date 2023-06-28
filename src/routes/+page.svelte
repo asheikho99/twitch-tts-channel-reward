73016bcf6531044363dd2140c14a8e4c132501f9
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-06-28 13:48:37 -0300

    Establishing basic websocket connection handling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,39 @@
 <script lang="ts">
-</script>
+	let webSocketEstablished = false;
+	let ws: WebSocket | null = null;
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	const establishWebSocket = () => {
+		if (webSocketEstablished) return;
+
+		ws = new WebSocket('wss://pubsub-edge.twitch.tv');
+
+		ws.addEventListener('open', (event) => {
+			webSocketEstablished = true;
+			const payload = {
+				type: 'LISTEN',
+				nonce: Date.now().toString(),
+				data: {
+					topics: [`channel-points-channel-v1.${import.meta.env.VITE_CHANNEL_ID}`],
+					auth_token: import.meta.env.VITE_ACCESS_TOKEN
+				}
+			};
+			if (ws) ws.send(JSON.stringify(payload));
+			console.log('connection open', event);
+		});
+
+		ws.addEventListener('message', (event) => {
+			console.log('message', event);
+		});
+
+		ws.addEventListener('close', (event) => {
+			console.log('connection closed', event);
+		});
+
+		ws.addEventListener('error', (event) => {
+			console.error('WebSocket error', event);
+		});
+	};
+</script>
+<button disabled={webSocketEstablished} on:click={() => establishWebSocket()}>
+	Establish WebSocket connection
+</button>

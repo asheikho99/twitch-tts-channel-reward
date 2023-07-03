415c667c5ca437824eb8ef42aa6f1b21e35a4d79
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-07-02 22:10:43 -0300

    Optimized error handling in webSocketMessage

--- src/events/websocket/onMessageError.ts
+++ src/events/websocket/onMessageError.ts
@@ -1,18 +1,7 @@
 import type { WebSocketMessageError } from '../../types/WebSocketMessage';
 
 export const onMessageError = (webSocketMessage: WebSocketMessageError) => {
-	if ('error' in webSocketMessage) {
-		switch (webSocketMessage.error) {
-			case 'ERR_BADMESSAGE':
-				break;
-			case 'ERR_BADAUTH':
-				break;
-			case 'ERR_SERVER':
-				break;
-			case 'ERR_BADTOPIC':
-				break;
-			default:
-				console.log(webSocketMessage.error);
-		}
+	if ('error' in webSocketMessage && webSocketMessage.error != '') {
+		console.error(webSocketMessage.error);
 	}
 };

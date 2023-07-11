d60eec2849027052d50bfd3f02a9b39daa8ba34a
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-07-11 10:49:48 -0300

    Show user message on frontend

--- src/events/websocket/message/onNotification.ts
+++ src/events/websocket/message/onNotification.ts
@@ -1,5 +1,12 @@
 import type { NotificationMessage } from '../../../interfaces/NotificationMessage';
+import { userStore } from '../../../stores/userStore';
 
 export function onNotification(message: NotificationMessage) {
 	console.log('notification', message);
+
+	const event = message.payload.event;
+	const username = event.user_name;
+	const userInput = event.user_input;
+
+	userStore.set({ username: username, userInput: userInput });
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,65 +1,16 @@
 <script lang="ts">
-	// import { getRandomNonce } from '$lib/getRandomNonce';
-	// import { onClose } from '../events/websocket/onClose';
-	// import { onError } from '../events/websocket/onError';
-	// import { onMessage } from '../events/websocket/onMessage';
-	// import { onMessageError } from '../events/websocket/onMessageError';
-	// import type { WebSocketMessage, WebSocketMessageError } from '../types/WebSocketMessage';
+	import { userStore } from '../stores/userStore';
 
-	// let webSocketEstablished = false;
-	// let ws: WebSocket | null = null;
-	// let audioPlayer: HTMLAudioElement;
-	// let audioURL: string;
+	let username: string;
+	let userInput: string;
 
-	// const establishWebSocket = () => {
-	// 	if (webSocketEstablished) return;
-
-	// 	ws = new WebSocket('wss://pubsub-edge.twitch.tv');
-
-	// 	ws.addEventListener('open', (event) => {
-	// 		webSocketEstablished = true;
-	// 		const payload = {
-	// 			type: 'LISTEN',
-	// 			nonce: getRandomNonce(15),
-	// 			data: {
-	// 				topics: [`channel-points-channel-v1.${import.meta.env.VITE_CHANNEL_ID}`],
-	// 				auth_token: import.meta.env.VITE_ACCESS_TOKEN
-	// 			}
-	// 		};
-	// 		if (ws) ws.send(JSON.stringify(payload));
-	// 		console.log('connection open', event);
-	// 	});
-
-	// 	ws.addEventListener('message', async (event: MessageEvent<string>) => {
-	// 		const webSocketMessage: WebSocketMessage | WebSocketMessageError = JSON.parse(event.data);
-	// 		switch (webSocketMessage.type) {
-	// 			case 'RESPONSE':
-	// 				onMessageError(webSocketMessage as WebSocketMessageError);
-	// 				break;
-	// 			case 'MESSAGE':
-	// 				const userInput = onMessage(webSocketMessage as WebSocketMessage);
-	// 				const audioRes = await fetch(
-	// 					`https://api.streamelements.com/kappa/v2/speech?voice=Naayf&text=${userInput}`
-	// 				)
-	// 					.then(async (res) => await res.blob())
-	// 					.then((audioBlob) => {
-	// 						audioURL = URL.createObjectURL(audioBlob);
-	// 						audioPlayer.addEventListener('loadeddata', () => {
-	// 							audioPlayer.play();
-	// 						});
-	// 					});
-	// 				break;
-	// 			default:
-	// 				console.log(webSocketMessage.type);
-	// 		}
-	// 	});
-
-	// 	ws.addEventListener('close', onClose);
-	// 	ws.addEventListener('error', onError);
-	// };
+	$: userStore.subscribe((data) => {
+		username = data.username;
+		userInput = data.userInput;
+		console.log(data);
+	});
 </script>
 
-<!-- <button disabled={webSocketEstablished} on:click={() => establishWebSocket()}>
-	Establish WebSocket connection
-</button>
-<audio src={audioURL} bind:this={audioPlayer} /> -->
+<p>USER: {username}</p>
+<p>MSG: {userInput}</p>
+

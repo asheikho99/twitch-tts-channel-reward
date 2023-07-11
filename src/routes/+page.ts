7af91bd115dc578e32bf5e8326b0adbb639dd3f7
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-07-10 22:48:10 -0300

    load function error handling

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -7,13 +7,17 @@ import { onOpen } from '../events/websocket/onOpen';
 export const load = async () => {
 	let websocket: WebSocket | null = null;
 
-	if (browser) {
-		websocket = new WebSocket('ws://127.0.0.1:8080/ws');
-		
-		onOpen({ websocket });
-		onMessage({ websocket });
-		onError({ websocket });
-		onClose({ websocket });
+	try {
+		if (browser) {
+			websocket = new WebSocket('wss://eventsub.wss.twitch.tv/ws');
+
+			onOpen({ websocket });
+			onMessage({ websocket });
+			onError({ websocket });
+			onClose({ websocket });
+		}
+	} catch (error) {
+		console.error('Error loading WebSocket: ', error);
 	}
 
 	return { websocket: websocket };

c28c5ab17368d459a08c7bf88b50e0ffc55eaca7
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-07-11 22:35:05 -0300

    TTS audio generation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,36 @@
 
 	let username: string;
 	let userInput: string;
+	let audioPlayer: HTMLAudioElement;
+	let audioURL: string;
 
 	$: userStore.subscribe((data) => {
 		username = data.username;
 		userInput = data.userInput;
-		console.log(data);
+
+		if (username != '' || userInput != '') {
+			generateTTS();
+		}
 	});
+
+	async function generateTTS() {
+		const audioRes = await fetch(
+			`https://api.streamelements.com/kappa/v2/speech?voice=ar-XA-Wavenet-C&text=${userInput}`
+		)
+			.then(async (res) => await res.blob())
+			.then((audioBlob) => {
+				audioURL = URL.createObjectURL(audioBlob);
+				audioPlayer.addEventListener('loadeddata', () => {
+					audioPlayer.play();
+				});
+			})
+			.catch((error) => {
+				throw new Error('Failed to generate TTS audio', error);
+			});
+	}
 </script>
 
 <p>USER: {username}</p>
 <p>MSG: {userInput}</p>
 
+<audio src={audioURL} bind:this={audioPlayer} />

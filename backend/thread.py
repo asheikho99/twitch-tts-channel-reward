411dfc53dc3819059ff8f298f7f9c9fb59671d05
asheikho99 <105573050+asheikho99@users.noreply.github.com>
2023-11-01 12:09:52 -0300

    RequestProcessor Class

--- backend/thread.py
+++ backend/thread.py
@@ -1,5 +1,41 @@
 import logging
+import threading
+import time
+import json
 
 # Logging formatting
 format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+
+# Holds all incoming redemption requests from Twtich endpoint
+redemptions_arr = [
+    '{ "name":"John", "age":30, "city":"New York"}', '{ "name":"Smith", "age":19, "city":"Los Angeles"}'
+]
+
+class RequestProcessor:
+    def __init__(self):
+        self.isProcessing = False
+        self.redemptions_arr = [
+            '{ "name":"John", "age":30, "city":"New York"}', '{ "name":"Smith", "age":19, "city":"Los Angeles"}'
+        ]
+
+    def request_loop(self):
+        while True:
+            # logging.info("Waiting for new request...")
+            if len(self.redemptions_arr) > 0 and not self.isProcessing:
+                x = threading.Thread(target=self.process)
+                x.start()
+            time.sleep(1)
+
+    def process(self):
+        self.isProcessing = True
+        logging.info('Processing request:')
+        print(json.loads(self.redemptions_arr[0]))
+        time.sleep(3)
+        self.redemptions_arr.pop(0)
+        logging.info('Completed!')
+        self.isProcessing = False
+
+
+processor = RequestProcessor()
+processor.request_loop()
